ff4ba67878a1d7bbdade570915b9cd9595677621
Kitiya <kate.kitiya@gmail.com>
2020-03-20 16:52:09 -0400

    Updates

---
+++ precache-manifest.19f5a367aa1aebfbb3493b15ca73ed0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57c3afc12be03d33372206e03de0802",
+    "url": "/pixabay-image-finder/index.html"
+  },
+  {
+    "revision": "09088b8630860dd19606",
+    "url": "/pixabay-image-finder/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "30f59544b7dedc3683ec",
+    "url": "/pixabay-image-finder/static/js/2.38e86452.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/pixabay-image-finder/static/js/2.38e86452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09088b8630860dd19606",
+    "url": "/pixabay-image-finder/static/js/main.d09744b4.chunk.js"
+  },
+  {
+    "revision": "8ed03dea25110f0bd3e9",
+    "url": "/pixabay-image-finder/static/js/runtime-main.79aebe55.js"
+  }
+]);
